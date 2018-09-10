14f2018236377e0d3c1e15c3e88f4d5f4d951102
M. Kapma <marcel_kapma@web.de>
2018-09-10 23:43:14 +0200

    Implementiere Iban Prototyp

--- GerIban/Iban.cs
+++ GerIban/Iban.cs
@@ -1,10 +1,44 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 
 namespace GerIban {
+  enum Spaces { with, without };
+
   class Iban {
+    const string GER = "1314";
+    string _blz, _knr;
+
+    public Iban(string blz, string knr) {
+      if(!Regex.IsMatch(blz, "^[0-9]{8}$")) {
+        throw new ArgumentException("", "blz");
+      }
+
+      if(!Regex.IsMatch(knr, "^[0-9]{1,10}$")) {
+        throw new ArgumentException("", "knr");
+      }
+
+      _blz = blz;
+      _knr = knr.PadLeft(10, '0');
+    }
+
+    public string ToString(Spaces format) {
+      if(Spaces.with == format) {
+        return ToStringWithSpaces();
+      }
+
+      return ToString();
+    }
+
+    private string ToStringWithSpaces() {
+      return Regex.Replace(ToString(), ".{4}", "$0 ");
+    }
+
+    public override string ToString() {
+      return "DE" + GetChecknum() + _blz + _knr;
+    }
+
+    private string GetChecknum() {
+      return (98 - (decimal.Parse(_blz + _knr + GER + "00") % 97)).ToString("00");
+    }
   }
 }
